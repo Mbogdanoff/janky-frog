45ab8c5ef63f13871617def7b3d63675e0d62659
Marie Bogdanoff <mabogdan@iu.edu>
2019-11-19 15:52:44 -0500

    Started on dialogue system

---
+++ Bogdanoff_Final/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text textbox;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       // if (other.gameObject.CompareTag("DialogueTrigger"))
+        //{
+          //  textbox.gameObject.SetActive(true);
+        //}
+    }
+}
